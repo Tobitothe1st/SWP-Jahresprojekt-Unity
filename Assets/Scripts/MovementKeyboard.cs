19baafef8afaec2af3b5c3745876b584d5ec06dd
Tobitothe1st <adamant869@gmail.com>
2021-10-17 01:09:15 +0200

    17102021
    enemy ai

--- Assets/Scripts/MovementKeyboard.cs
+++ Assets/Scripts/MovementKeyboard.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 public class MovementKeyboard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     public CharacterController controller;
 
     public float speed = 12f;

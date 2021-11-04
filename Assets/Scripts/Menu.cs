29c2a7ab76ff1abc238cbc14785287c608d49538
Tobitothe1st <adamant869@gmail.com>
2021-11-04 09:02:40 +0100

    04112021
    Third Person, Menu etc.

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public void continueGame()
+    {
+
+    }
+
+    public void settings()
+    {
+
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/OpenMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenMenu : MonoBehaviour
+{
+
+    public Camera cam1;
+    public Camera cam2;
+    public Canvas menu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam1.enabled = true;
+        cam2.enabled = false;
+        menu.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            cam1.enabled = !cam1.enabled;
+            cam2.enabled = !cam2.enabled;
+            menu.enabled = !menu.enabled;
+        }
+    }
+}

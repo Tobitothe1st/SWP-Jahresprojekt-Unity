59941a7580a8710ef83c0b04ceec7e50f36b759a
Tobitothe1st <adamant869@gmail.com>
2021-11-28 17:21:58 +0100

    kp
    kp

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
@@ -10,15 +11,22 @@ public class EnemyMovement : MonoBehaviour
     public float move = 5f;
     public float detectionRange = 20f;
 
+    NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
     void Update()
     {
 
         float range = Vector3.Distance(EnemyBody.position, player.position);
         float speed = move * Time.deltaTime;
+        agent.SetDestination(player.position);
+        //if (range <= detectionRange)
+        //{
+        //    //EnemyBody.position = Vector3.MoveTowards(EnemyBody.position, player.position, speed);
 
-        if (range <= detectionRange)
-        {
-                EnemyBody.position = Vector3.MoveTowards(EnemyBody.position, player.position, speed);
-        }
+        //}
     }
 }

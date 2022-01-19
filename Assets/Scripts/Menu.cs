fcc470ff2c76c661b7354f80dd46449d6a08d725
Tobitothe1st <adamant869@gmail.com>
2022-01-19 20:59:20 +0100

    Menu
    Small UI Changes Menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
+    public Camera cam1;
+    public Camera cam2;
+    public Canvas menu;
+
+
     public void continueGame()
     {
-
+        cam1.enabled = !cam1.enabled;
+        cam2.enabled = !cam2.enabled;
+        menu.enabled = !menu.enabled;
     }
 
     public void settings()

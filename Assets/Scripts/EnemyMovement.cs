c03371abdde89208f3af676d5940a1cc8557c323
Tobitothe1st <92016690+Tobitothe1st@users.noreply.github.com>
2022-02-24 09:05:20 +0100

    Ray Commit
    Raycast in Enemy Script for pulling to ground

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -8,21 +8,26 @@ public class EnemyMovement : MonoBehaviour
 
     public Transform EnemyBody;
     public Transform player;
+
     public float move = 5f;
     public float detectionRange = 20f;
     public float stopRange = 2f;
     public float despawnRange = 50f;
     public float ySubtraction = 1.9f;
 
-
-            
     private void Start()
     {
     }
     void Update()
     {
         //detects Range 
-        Vector3 clipToFloor = new Vector3(player.position.x, player.position.y-ySubtraction, player.position.z);
+        Ray ray = new Ray(EnemyBody.transform.position, -transform.up);
+        RaycastHit hit = new RaycastHit();
+        Physics.Raycast(ray, out hit);
+        Debug.DrawRay(EnemyBody.transform.position, -transform.up * 10);
+        Debug.Log(hit.distance);
+        Vector3 clipToFloor = new Vector3(player.position.x, player.position.y-hit.distance, player.position.z);
+
         float range = Vector3.Distance(EnemyBody.position, clipToFloor);
         float speed = move * Time.deltaTime;
 

41a70f7319eea235764b275e16ac3dfda07f22b2
Tobitothe1st <92016690+Tobitothe1st@users.noreply.github.com>
2022-05-18 11:03:00 +0200

    Combat and Enemy AI
    Started Combat System
    Improved Enemy AI (spawning and following works properly)

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,9 +6,8 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
 {
 
-    public Transform EnemyBody;
-    public GameObject player;
-    public GameObject playerBody;
+    public GameObject EnemyBody;
+    public Transform playerBody;
 
     public float move = 5f;
     public float detectionRange = 20f;
@@ -22,21 +21,21 @@ public class EnemyMovement : MonoBehaviour
     void Update()
     {
         //detects Range 
+        Vector3 target = new Vector3(playerBody.position.x, 0f, playerBody.position.z);
 
-        //find Solution for Enemy not following Player
-        Vector3 target = new Vector3(playerBody.Transfrom.position.x, 0f, player.position.z);
-
-        float range = Vector3.Distance(EnemyBody.position, target);
+        float range = Vector3.Distance(EnemyBody.transform.position, target);
         float speed = move * Time.deltaTime;
 
         //moves if player is in Range
         if (range < detectionRange && range > stopRange)
         {
-            EnemyBody.position = Vector3.MoveTowards(EnemyBody.position, target, speed);
+            EnemyBody.transform.position = Vector3.MoveTowards(EnemyBody.transform.position, target, speed);
+            //Look at Player, prevent from falling (set values to 0)
+            EnemyBody.transform.LookAt(playerBody);
         }
         else if(range > despawnRange)
         {
-            Destroy(EnemyBody.gameObject);
+            EnemyBody.SetActive(false);
         }
     }
 }
--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -25,15 +25,15 @@ public class EnemySpawn : MonoBehaviour
         Ray ray = new Ray(Enemy.transform.position, -transform.up);
         RaycastHit hit = new RaycastHit();
         Physics.Raycast(ray, out hit);
-        Debug.DrawRay(Enemy.transform.position, -transform.up * 10f, Color.red);
+        //Debug.DrawRay(Enemy.transform.position, -transform.up * 10f, Color.red);
 
         int randomSpawn = Random.Range(0, 50);
-        Debug.Log(randomSpawn.ToString());
+        //Debug.Log(randomSpawn.ToString());
         if (randomSpawn == 25)
         {
             float randomX = Random.Range(negativeRadius, positiveRadius);
             float randomZ = Random.Range(negativeRadius, positiveRadius);
-            Vector3 randomPos = new Vector3(Player.position.x - randomX, 50, Player.position.x - randomZ);
+            Vector3 randomPos = new Vector3(Player.position.x - randomX, 10, Player.position.x - randomZ);
             Quaternion zero = new Quaternion(0, 0, 0, 0);
             Enemy = Instantiate(Enemy, randomPos, zero);
             this.Enemy.name = "Enemy " + count;
